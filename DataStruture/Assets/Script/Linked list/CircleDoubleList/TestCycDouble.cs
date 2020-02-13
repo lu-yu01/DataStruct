9f1a7bbc4b0977cb17dc70e276fa8c7cb8238a03
DESKTOP-05M0UKD\gaojia <jia.gao@appmagics.com>
2020-02-13 11:41:16 +0800

    add CircleDoubleList

---
+++ DataStruture/Assets/Script/Linked list/CircleDoubleList/TestCycDouble.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DataStruct;
+public class TestCycDouble : MonoBehaviour {
+
+	// Use this for initialization
+    private Button Prebtn;
+    private Button NextBtn;
+    private Node<int> node;
+
+    void Start ()
+	{
+	    Prebtn = transform.Find("Previous").GetComponent<Button>();
+	    NextBtn = transform.Find("Next").GetComponent<Button>();
+	    Prebtn.onClick.AddListener(OnPrebtnListen);
+	    NextBtn.onClick.AddListener(NextBtnListen);
+
+	    CircleLink<int> circlelink = new CircleLink<int>();
+	    for (int i = 0; i < 10; i++)
+	    {
+	        circlelink.AddElem(i*2);
+        }
+	    node =  circlelink.GetNode(1);
+	    Debug.Log("" + node.Data);
+    }
+
+    public void OnPrebtnListen()
+    {
+        node = node.PreNode;
+        Debug.Log("" + node.Data);
+    }
+    public void NextBtnListen()
+    {
+        node = node.NextNode;
+        Debug.Log("" + node.Data);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
