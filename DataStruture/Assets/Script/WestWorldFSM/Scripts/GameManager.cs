3e474fede5a81814e70f4213a936021cabc12943
jiagao-PC\jia.gao <jia.gao@appmagics.com>
2020-04-16 15:13:36 +0800

    add westWorld

---
+++ DataStruture/Assets/Script/WestWorldFSM/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float UpdateTimeInterval = 1;
+    private Miner miner;
+    private MinersWife minersWife;
+
+    private void Start()
+    {
+        miner = new Miner(1, "Bob");
+        minersWife = new MinersWife(2, "Elsa");
+        
+        StartCoroutine(MinerStartToWork());
+    }
+
+    IEnumerator MinerStartToWork()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            miner.EntityUpdate();
+            minersWife.EntityUpdate();
+            yield return new WaitForSeconds(UpdateTimeInterval);
+        }
+    }
+}
